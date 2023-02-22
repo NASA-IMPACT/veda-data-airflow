983f0fb86cb6393f7da1759f1d2020332ba37e4a
Abdel Marouane <abdelmarouane@abdel22-mbp.local>
2023-02-22 15:42:59 -0600

    Without AWS_profile

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,26 +16,22 @@ fi
 function create_state_bucket {
   # $1 region
   # $2 bucket_name
-  # $3 aws_profile
 
   aws s3 mb  s3://$2  --region $1
   aws s3api put-bucket-versioning \
     --bucket $2 \
-    --versioning-configuration Status=Enabled \
-    --profile $3
+    --versioning-configuration Status=Enabled
 }
 
 function create_dynamo_db {
   # $1 region
   # $2 table_name
-  # $3 aws_profile
   aws dynamodb create-table \
     --table-name $2 \
     --attribute-definitions AttributeName=LockID,AttributeType=S \
     --key-schema AttributeName=LockID,KeyType=HASH \
     --billing-mode PAY_PER_REQUEST \
-    --region $1 \
-    --profile $3
+    --region $1
 
 }
 function generate_terraform_variables {
@@ -54,20 +50,17 @@ function check_create_remote_state {
   # $1 aws_region
   # $2 bucket name
   # $3 dynamotable_name
-  # $4 aws profile
   AWS_REGION=$1
   STATE_BUCKET_NAME=$2
   STATE_DYNAMO_TABLE=$3
-  AWS_PROFILE=$4
 
-  bucketstatus=$(aws s3api head-bucket --bucket $STATE_BUCKET_NAME --profile $AWS_PROFILE 2>&1)
-  table_exists=$(aws dynamodb describe-table --table-name  $STATE_DYNAMO_TABLE --region $AWS_REGION --profile $AWS_PROFILE 2>&1)
-  echo $bucketstatus
-  echo $table_exists
+  bucketstatus=$(aws s3api head-bucket --bucket $STATE_BUCKET_NAME  2>&1)
+  table_exists=$(aws dynamodb describe-table --table-name  $STATE_DYNAMO_TABLE --region $AWS_REGION  2>&1)
+
   if echo "${table_exists}" | grep "An error";
   then
     echo "Creating dynamodb table for TF state"
-    create_dynamo_db $AWS_REGION $STATE_DYNAMO_TABLE $AWS_PROFILE
+    create_dynamo_db $AWS_REGION $STATE_DYNAMO_TABLE
   else
     echo "DynamoDB $STATE_DYNAMO_TABLE exists. Continue..."
   fi
@@ -75,8 +68,8 @@ function check_create_remote_state {
   if echo "${bucketstatus}" | grep 'Not Found';
   then
         echo "Creating TF remote state"
-        create_state_bucket $AWS_REGION $STATE_BUCKET_NAME $AWS_PROFILE
-        create_dynamo_db $AWS_REGION $STATE_DYNAMO_TABLE $AWS_PROFILE
+        create_state_bucket $AWS_REGION $STATE_BUCKET_NAME
+        create_dynamo_db $AWS_REGION $STATE_DYNAMO_TABLE
   elif echo "${bucketstatus}" | grep 'Forbidden';
   then
     echo "Bucket $STATE_BUCKET_NAME exists but not owned"
@@ -95,7 +88,7 @@ function check_create_remote_state {
 cd ./infrastructure
 generate_terraform_variables
 cat terraform.tf
-check_create_remote_state $AWS_REGION $STATE_BUCKET_NAME $STATE_DYNAMO_TABLE $AWS_PROFILE
+check_create_remote_state $AWS_REGION $STATE_BUCKET_NAME $STATE_DYNAMO_TABLE
 
 read -rp 'action [init|plan|deploy]: ' ACTION
 case $ACTION in

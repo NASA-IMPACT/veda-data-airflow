1bd28ea91e88ab661e329fd9ddbdc82a67bf8d09
Slesa Adhikari <slesaad@gmail.com>
2023-05-09 12:51:12 -0500

    Update github actions and update bucket permissions (#2)
    * Update GitHub workflows to use composite action
    * Update buckets permissions
    ---------
    
    Co-authored-by: Caden Helbling <caden.helbling@gmail.com>

--- infrastructure/custom_policies/main.tf
+++ infrastructure/custom_policies/main.tf
@@ -70,30 +70,26 @@ data "aws_iam_policy_document" "mwaa_executor_policies" {
     ]
     resources = var.assume_role_arns
   }
+
   statement {
     effect = "Allow"
     actions = [
       "s3:GetObject*",
       "s3:GetBucket*",
       "s3:List*",
-      "s3:DeleteObject*",
       "s3:PutObject",
       "s3:PutObjectLegalHold",
       "s3:PutObjectRetention",
       "s3:PutObjectTagging",
       "s3:PutObjectVersionTagging",
-      "s3:Abort*"
     ]
     resources = [
-      "arn:aws:s3:::veda-data-pipelines-staging-lambda-ndjson-bucket",
-      "arn:aws:s3:::veda-data-pipelines-staging-lambda-ndjson-bucket/*",
-      "arn:aws:s3:::veda-data-read-staging",
-      "arn:aws:s3:::veda-data-read-staging/*",
-      "arn:aws:s3:::veda-data-store-staging",
-      "arn:aws:s3:::veda-data-store-staging/*",
-      "arn:aws:s3:::nex-gddp-cmip6-cog",
-      "arn:aws:s3:::nex-gddp-cmip6-cog/*",
-
+      "arn:aws:s3:::ghgc-data-store-dev",
+      "arn:aws:s3:::ghgc-data-store-dev/*",
+      "arn:aws:s3:::ghgc-data-store-staging",
+      "arn:aws:s3:::ghgc-data-store-staging/*",
+      "arn:aws:s3:::ghgc-data-store",
+      "arn:aws:s3:::ghgc-data-store/*",
     ]
   }
 
@@ -105,14 +101,7 @@ data "aws_iam_policy_document" "mwaa_executor_policies" {
       "s3:List*"
     ]
     resources = [
-      "arn:aws:s3:::climatedashboard-data",
-      "arn:aws:s3:::climatedashboard-data/*",
-      "arn:aws:s3:::veda-data-store-staging",
-      "arn:aws:s3:::veda-data-store-staging/*",
-      "arn:aws:s3:::nasa-maap-data-store",
-      "arn:aws:s3:::nasa-maap-data-store/*",
-      "arn:aws:s3:::covid-eo-blackmarble",
-      "arn:aws:s3:::covid-eo-blackmarble/*"
+      "*",
     ]
   }
 
@@ -120,9 +109,7 @@ data "aws_iam_policy_document" "mwaa_executor_policies" {
     effect    = "Allow"
     actions   = ["airflow:CreateCliToken"]
     resources = [var.mwaa_arn]
-
   }
-
 }
 
 

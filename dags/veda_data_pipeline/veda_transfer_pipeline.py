157270c026d65fb4aff787e70143b22a3223f1cc
Nathan Zimmerman <npzimmerman@gmail.com>
2023-06-28 10:14:28 -0500

    Add separate file transfer pipeline; remove submodule

---
+++ dags/veda_data_pipeline/veda_transfer_pipeline.py
@@ -0,0 +1,72 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.trigger_rule import TriggerRule
+from veda_data_pipeline.groups.discover_group import subdag_discover
+
+dag_doc_md = """
+### Discover files from S3
+#### Purpose
+This DAG discovers files from either S3 and/or CMR then runs a DAG id `veda_ingest`. 
+The DAG `veda_ingest` will run in parallel processing (2800 files per each DAG)
+#### Notes
+- This DAG can run with the following configuration <br>
+```json
+{
+    "collection": "collection-id",
+    "bucket": "veda-data-store-staging",
+    "prefix": "s3-prefix/",
+    "filename_regex": "^(.*).tif$",
+    "datetime_group": ".*_(.*).tif$",
+    "datetime_range": "month",
+    "assets": {
+        "asset1": {
+            "title": "Asset type 1",
+            "description": "First of a multi-asset item.",
+            "regex": ".*asset1.*",
+        },
+        "asset2": {
+            "title": "Asset type 2",
+            "description": "Second of a multi-asset item.",
+            "regex": ".*asset2.*",
+        },
+    }
+}	
+```
+- [Supports linking to external content](https://github.com/NASA-IMPACT/veda-data-pipelines)
+"""
+
+dag_args = {
+    "start_date": pendulum.today("UTC").add(days=-1),
+    "schedule_interval": None,
+    "catchup": False,
+    "doc_md": dag_doc_md,
+}
+
+templat_dag_run_conf = {
+    "bucket": "<bucket>",
+    "prefix": "<prefix>/",
+    "filename_regex": "<file_regex>",
+    "datetime_group": "<datetime_group_regex>",
+    "datetime_range": "<year>|<month>|<day>",
+    "assets": {
+        "<asset1_name>": {
+            "title": "<asset_title>",
+            "description": "<asset_description>",
+            "regex": "<asset_regex>",
+        },
+        "<asset1_name>": {
+            "title": "<asset_title>",
+            "description": "<asset_description>",
+            "regex": "<asset_regex>",
+        }
+    }
+}
+
+with DAG("veda_transfer", params=templat_dag_run_conf, **dag_args) as dag:
+    start = DummyOperator(task_id="Start", dag=dag)
+    end = DummyOperator(task_id="End", trigger_rule=TriggerRule.ONE_SUCCESS, dag=dag)
+
+    discover_grp = subdag_discover()
+
+    start >> discover_grp >> end

d2f533906e245484056405d655b28b106adae579
ividito <isayah@developmentseed.org>
2024-02-16 20:40:23 -0400

    Failed dockerfile :(

--- workflows_api/runtime/local.py
+++ workflows_api/runtime/local.py
@@ -4,4 +4,4 @@ import uvicorn
 from src import main
 
 if __name__ == "__main__":
-    uvicorn.run(main.app, host="0.0.0.0", port=8000)
+    uvicorn.run(main.workflows_app, host="0.0.0.0", port=8000)

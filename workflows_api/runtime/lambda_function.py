285efdb0f3ba5befa054b37dd1f6669337104245
Isayah Vidito <isayah@developmentseed.org>
2024-02-22 14:02:11 -0500

    fix(cloudfront): fixes domain routing, removes debugging lines, small config tweaks

--- workflows_api/runtime/lambda_function.py
+++ workflows_api/runtime/lambda_function.py
@@ -5,4 +5,4 @@ Entrypoint for Lambda execution.
 from mangum import Mangum
 from src.main import workflows_app
 
-handler = Mangum(workflows_app, lifespan="off")
+handler = Mangum(workflows_app, lifespan="off", api_gateway_base_path=workflows_app.root_path)

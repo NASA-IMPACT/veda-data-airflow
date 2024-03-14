9f3cb90d67cc8f5e76373adb4f9b496cf8c2d5f9
smohiudd <saadiq@developmentseed.org>
2024-03-14 13:24:18 -0600

    update error handling

--- dags/veda_data_pipeline/utils/transfer.py
+++ dags/veda_data_pipeline/utils/transfer.py
@@ -58,8 +58,9 @@ def transfer_files_within_s3(
                 Bucket=destination_bucket, Key=target_key
             )
             target_etag = target_metadata["ETag"]
-        except s3_client.exceptions.ClientError:
-            target_etag = ""
+        except s3_client.exceptions.ClientError as err:
+            if err.response["Error"]["Code"] == "404":
+                target_etag = ""
 
         s3_client.copy_object(
             CopySource=copy_source,

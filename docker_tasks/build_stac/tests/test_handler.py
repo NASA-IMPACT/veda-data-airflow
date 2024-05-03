198fea683efca4e63746ce02efc7c2385767e18b
Stephen Kilbourn <stephenkilbourn@gmail.com>
2024-05-03 09:52:21 -0600

    fix existing tests to run and not include in coverage report (#144)
    Co-authored-by: Stephen Kilbourn <skilbourn@element84.com>

--- docker_tasks/build_stac/tests/test_handler.py
+++ docker_tasks/build_stac/tests/test_handler.py
@@ -21,72 +21,25 @@ def build_mock_stac_item(item: Dict[str, Any]) -> MagicMock:
     return expected_stac_item
 
 
-@contextlib.contextmanager
-def override_registry(
-    dispatch_callable: "_SingleDispatchCallable[Any]", cls: Type, mock: Mock
-):
-    """
-    Helper to override a singledispatch function with a mock for testing.
-    """
-    original = dispatch_callable.registry[cls]
-    dispatch_callable.register(cls, mock)
-    try:
-        yield mock
-    finally:
-        dispatch_callable.register(cls, original)
-
-
 def test_routing_regex_event():
     """
     Ensure that the system properly identifies, classifies, and routes regex-style events.
     """
-    regex_event = {
-        "collection": "test-collection",
-        "s3_filename": "s3://test-bucket/delivery/BMHD_Maria_Stages/70001_BeforeMaria_Stage0_2017-07-21.tif",
-        "granule_id": None,
-        "datetime_range": None,
-        "start_datetime": None,
-        "end_datetime": None,
+    regex_event =  {
+        "collection": "TEST_COLLECTION",
+        "item_id": "test_2024-04-24.tif",
+        "assets": {
+            "TEST_FILE": {
+                "title": "Test_FILE",
+                "description": "TEST_FILE, described",
+                "href": "./docker_tasks/build_stac/tests/test_2024-04-24.tif",
+            }
+        }
     }
+    response = handler.handler(regex_event)
+    print(response)
 
-    with override_registry(
-        stac.generate_stac,
-        events.RegexEvent,
-        MagicMock(return_value=build_mock_stac_item({"mock": "STAC Item 1"})),
-    ) as called_mock, override_registry(
-        stac.generate_stac,
-        events.CmrEvent,
-        MagicMock(),
-    ) as not_called_mock:
-        handler.handler(regex_event, None)
-
-    called_mock.assert_called_once_with(events.RegexEvent.parse_obj(regex_event))
-    assert not not_called_mock.call_count
-
-
-def test_routing_cmr_event():
-    """
-    Ensure that the system properly identifies, classifies, and routes CMR-style events.
-    """
-    cmr_event = {
-        "collection": "test-collection",
-        "s3_filename": "s3://test-bucket/delivery/BMHD_Maria_Stages/70001_BeforeMaria_Stage0_2017-07-21.tif",
-        "granule_id": "test-granule",
-    }
-
-    with override_registry(
-        stac.generate_stac,
-        events.CmrEvent,
-        MagicMock(return_value=build_mock_stac_item({"mock": "STAC Item 1"})),
-    ) as called_mock, override_registry(
-        stac.generate_stac,
-        events.RegexEvent,
-        MagicMock(),
-    ) as not_called_mock:
-        handler.handler(cmr_event, None)
-
-    called_mock.assert_called_once_with(events.CmrEvent.parse_obj(cmr_event))
-    assert not not_called_mock.call_count
+    assert response
 
 
 @pytest.mark.parametrize(
@@ -98,4 +51,4 @@ def test_routing_unexpected_event(bad_event):
     Ensure that a malformatted event raises a validation error
     """
     with pytest.raises(ValidationError):
-        handler.handler(bad_event, None)
+        handler.handler(bad_event)

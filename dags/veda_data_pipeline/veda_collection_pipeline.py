15ba0322493bf1150784583a32db50cd301bcfa4
ividito <isayah@developmentseed.org>
2024-06-18 10:59:55 -0300

    Add collection-only dag

---
+++ dags/veda_data_pipeline/veda_collection_pipeline.py
@@ -0,0 +1,26 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator as EmptyOperator
+from airflow.utils.trigger_rule import TriggerRule
+from veda_data_pipeline.groups.collection_group import collection_task_group
+
+dag_doc_md = """
+### Dataset Pipeline
+Generates a collection and triggers the file discovery process 
+"""
+
+dag_args = {
+    "start_date": pendulum.today("UTC").add(days=-1),
+    "schedule_interval": None,
+    "catchup": False,
+    "doc_md": dag_doc_md,
+    "tags": ["collection", "discovery"],
+}
+
+with DAG("veda_dataset_pipeline", **dag_args) as dag:
+    start = EmptyOperator(task_id="start", dag=dag)
+    end = EmptyOperator(task_id="end", trigger_rule=TriggerRule.ONE_SUCCESS, dag=dag)
+
+    collection_grp = collection_task_group()
+
+    start >> collection_grp >> end

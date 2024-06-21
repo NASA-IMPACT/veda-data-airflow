da74dd4d4b19b0afea32a335826325696de6b574
Stephen Kilbourn <skilbourn@element84.com>
2024-06-21 11:15:55 -0600

    re-add acyclic dag test

--- tests/test_dag_integrity.py
+++ tests/test_dag_integrity.py
@@ -1,10 +1,19 @@
 """Test integrity of dags."""
-
 import importlib
 import os
 import pytest
 
+from os.path import dirname
+
 from airflow.models import DagBag
+from airflow import models as af_models
+from airflow.utils.dag_cycle_tester import check_cycle
+
+DAG_PATH = os.path.join(
+    dirname(dirname(__file__)), 'dags/veda_data_pipeline'
+)
+
+DAG_FILES = [f for f in os.listdir(DAG_PATH) if f.endswith('.py')]
 
 @pytest.fixture()
 def dag_bag():
@@ -25,3 +34,17 @@ def test_dags_exists(dag_bag):
         dags.append(dag_id)
 
     assert len(dags) > 0
+
+@pytest.mark.parametrize('dag_file', DAG_FILES)
+def test_dag_integrity(dag_file):
+    """Import dag files and check for DAG."""
+    module_name, _ = os.path.splitext(dag_file)
+    module_path = os.path.join(DAG_PATH, dag_file)
+    mod_spec = importlib.util.spec_from_file_location(module_name, module_path)
+    module = importlib.util.module_from_spec(mod_spec)
+    mod_spec.loader.exec_module(module)
+
+    dag_objects = [var for var in vars(module).values() if isinstance(var, af_models.DAG)]
+
+    for dag in dag_objects:
+        check_cycle(dag)

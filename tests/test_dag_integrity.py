9a6716acb32a6f9e52e54e23efa05b106a041ab3
Stephen Kilbourn <skilbourn@element84.com>
2024-06-21 14:24:26 -0600

    remove moto s3 mock only used for generate dags

--- tests/test_dag_integrity.py
+++ tests/test_dag_integrity.py
@@ -1,5 +1,4 @@
 """Test integrity of dags."""
-import boto3
 import importlib
 import os
 import pytest
@@ -9,17 +8,6 @@ from os.path import dirname
 from airflow.models import DagBag
 from airflow import models as af_models
 from airflow.utils.dag_cycle_tester import check_cycle
-from moto import mock_s3
-
-
-@pytest.fixture(scope='function')
-def aws_credentials():
-    """Mocked AWS Credentials, to ensure we're not touching AWS directly"""
-    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
-    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
-    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
-    os.environ['AWS_SESSION_TOKEN'] = 'testing'
-    os.environ['EVENT_BUCKET'] = 'test'
 
 DAG_PATH = os.path.join(
     dirname(dirname(__file__)), 'dags/veda_data_pipeline'
@@ -28,14 +16,8 @@ DAG_PATH = os.path.join(
 DAG_FILES = [f for f in os.listdir(DAG_PATH) if f.endswith('.py')]
 
 @pytest.fixture()
-@mock_s3
-def dag_bag(aws_credentials):
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket("test")
-    # Create the bucket first, as we're interacting with an empty mocked 'AWS account'
-    bucket.create(
-      CreateBucketConfiguration={'LocationConstraint': 'us-west-2'}
-    ) 
+def dag_bag():
+
     return DagBag(dag_folder="dags/veda_data_pipeline", include_examples=False)
 
 def test_import_dags(dag_bag, ):
@@ -51,7 +33,6 @@ def test_dags_exists(dag_bag):
     dags = list()
     for dag_id, _ in dag_bag.dags.items():
         dags.append(dag_id)
-
     assert len(dags) > 0
 
 @pytest.mark.parametrize('dag_file', DAG_FILES)

149501c96265b28595f22325913c0d47608c087a
Slesa Adhikari <slesaad@gmail.com>
2024-06-27 11:11:30 -0500

    Refactor

--- workflows_api/runtime/src/airflow_helpers.py
+++ workflows_api/runtime/src/airflow_helpers.py
@@ -1,5 +1,4 @@
 import base64
-import os
 from typing import Dict
 from uuid import uuid4
 
@@ -8,21 +7,19 @@ import requests
 import json
 from fastapi import HTTPException
 
-try:
-    from src.schemas import WorkflowExecutionResponse, Status, ListWorkflowsResponse
-except ImportError:
-    from src.schemas import WorkflowExecutionResponse, Status, ListWorkflowsResponse
+from src.config import settings
+from src.schemas import WorkflowExecutionResponse, Status, ListWorkflowsResponse
 
 
 def cli_input(cli_string: str) -> Dict:
     """
     Pass a command directly to the CLI. Requires auth.
     """
-    if not (MWAA_ENV := os.environ.get("MWAA_ENV")):
+    if not (mwaa_env := settings.mwaa_env):
         raise HTTPException(status_code=400, detail="MWAA environment not set")
 
     airflow_client = boto3.client("mwaa")
-    mwaa_cli_token = airflow_client.create_cli_token(Name=MWAA_ENV)
+    mwaa_cli_token = airflow_client.create_cli_token(Name=mwaa_env)
 
     mwaa_webserver_hostname = (
         f"https://{mwaa_cli_token['WebServerHostname']}/aws_mwaa/cli"
@@ -46,11 +43,11 @@ def cli_input(cli_string: str) -> Dict:
 
 
 def trigger_discover(input: Dict) -> Dict:
-    if not (MWAA_ENV := os.environ.get("MWAA_ENV")):
+    if not (mwaa_env := settings.mwaa_env):
         raise HTTPException(status_code=400, detail="MWAA environment not set")
 
     airflow_client = boto3.client("mwaa")
-    mwaa_cli_token = airflow_client.create_cli_token(Name=MWAA_ENV)
+    mwaa_cli_token = airflow_client.create_cli_token(Name=mwaa_env)
 
     mwaa_webserver_hostname = (
         f"https://{mwaa_cli_token['WebServerHostname']}/aws_mwaa/cli"
@@ -81,11 +78,11 @@ def trigger_discover(input: Dict) -> Dict:
 
 
 def list_dags() -> Dict:
-    if not (MWAA_ENV := os.environ.get("MWAA_ENV")):
+    if not (mwaa_env := settings.mwaa_env):
         raise HTTPException(status_code=400, detail="MWAA environment not set")
 
     airflow_client = boto3.client("mwaa")
-    mwaa_cli_token = airflow_client.create_cli_token(Name=MWAA_ENV)
+    mwaa_cli_token = airflow_client.create_cli_token(Name=mwaa_env)
 
     mwaa_webserver_hostname = (
         f"https://{mwaa_cli_token['WebServerHostname']}/aws_mwaa/cli"
@@ -131,11 +128,11 @@ def get_status(dag_run_id: str) -> Dict:
     """
     Get the status of a veda_discover workflow execution by dag_id
     """
-    if not (MWAA_ENV := os.environ.get("MWAA_ENV")):
+    if not (mwaa_env := settings.mwaa_env):
         raise HTTPException(status_code=400, detail="MWAA environment not set")
 
     airflow_client = boto3.client("mwaa")
-    mwaa_cli_token = airflow_client.create_cli_token(Name=MWAA_ENV)
+    mwaa_cli_token = airflow_client.create_cli_token(Name=mwaa_env)
 
     mwaa_webserver_hostname = (
         f"https://{mwaa_cli_token['WebServerHostname']}/aws_mwaa/cli"

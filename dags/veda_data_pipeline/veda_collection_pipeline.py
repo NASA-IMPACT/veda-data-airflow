bb362f92a5b343e7616cdb4a86dde5139f71ec9b
smohiudd <saadiq@developmentseed.org>
2024-07-09 11:43:31 -0600

    add default config template

--- dags/veda_data_pipeline/veda_collection_pipeline.py
+++ dags/veda_data_pipeline/veda_collection_pipeline.py
@@ -5,8 +5,21 @@ from airflow.utils.trigger_rule import TriggerRule
 from veda_data_pipeline.groups.collection_group import collection_task_group
 
 dag_doc_md = """
-### Dataset Pipeline
-Generates a collection and triggers the file discovery process 
+### Collection Creation and Ingestion
+Generates a collection based on the Dataset model and ingests into the catalog 
+#### Notes
+- This DAG can run with the following configuration <br>
+```json
+{
+    "collection": "collection-id", 
+    "data_type": "cog", 
+    "description": "collection description",
+    "is_periodic": true, 
+    "license": "collection-LICENSE",
+    "time_density": "year", 
+    "title": "collection-title"
+}
+```
 """
 
 dag_args = {
@@ -17,7 +30,17 @@ dag_args = {
     "tags": ["collection", "discovery"],
 }
 
-with DAG("veda_dataset_pipeline", **dag_args) as dag:
+template_dag_run_conf = {
+    "collection": "<collection-id>", 
+    "data_type": "cog", 
+    "description": "<collection-description>", 
+    "is_periodic": "<true|false>", 
+    "license": "<collection-LICENSE>", 
+    "time_density": "<time-density>", 
+    "title": "<collection-title>"
+}
+
+with DAG("veda_dataset_pipeline", params=template_dag_run_conf, **dag_args) as dag:
     start = EmptyOperator(task_id="start", dag=dag)
     end = EmptyOperator(task_id="end", trigger_rule=TriggerRule.ONE_SUCCESS, dag=dag)
 

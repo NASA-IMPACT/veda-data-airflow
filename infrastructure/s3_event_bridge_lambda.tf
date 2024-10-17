ebb999365d71f4d80fb2695d80b0aabe0297dc1f
ividito <isayah@developmentseed.org>
2024-10-17 16:42:25 -0300

    fix: feature flag for vector automation lambda

--- infrastructure/s3_event_bridge_lambda.tf
+++ infrastructure/s3_event_bridge_lambda.tf
@@ -1,7 +1,17 @@
 #####################################################
+# Variables
+#####################################################
+variable "DEPLOY_VECTOR_AUTOMATION" {
+  type    = bool
+  default = false
+}
+
+#####################################################
 # Execution Role
 #####################################################
 resource "aws_iam_role" "lambda_exec_role" {
+  count = var.DEPLOY_VECTOR_AUTOMATION ? 1 : 0
+
   provider = aws.aws_current
   name = "lambda-exec-role-s3-event-bridge-veda-${var.stage}"
 
@@ -26,6 +36,8 @@ EOF
 # Logging
 ###############################
 resource "aws_iam_policy" "lambda_logging" {
+  count = var.DEPLOY_VECTOR_AUTOMATION ? 1 : 0
+
   provider = aws.aws_current
   name        = "lambda-logging-veda-${var.stage}"
   path        = "/"
@@ -50,15 +62,19 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  count = var.DEPLOY_VECTOR_AUTOMATION ? 1 : 0
+
   provider = aws.aws_current
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = aws_iam_policy.lambda_logging.arn
+  role       = aws_iam_role.lambda_exec_role[0].name
+  policy_arn = aws_iam_policy.lambda_logging[0].arn
 }
 
 ###############################
 # SFN StartExecution Policy
 ###############################
 resource "aws_iam_policy" "lambda_sfn_start_exec" {
+  count = var.DEPLOY_VECTOR_AUTOMATION ? 1 : 0
+
   provider = aws.aws_current
   name        = "lambda-startexec-on-sfn-veda-${var.stage}"
   path        = "/"
@@ -81,15 +97,19 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_sfn_start_exec" {
+  count = var.DEPLOY_VECTOR_AUTOMATION ? 1 : 0
+
   provider = aws.aws_current
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = aws_iam_policy.lambda_sfn_start_exec.arn
+  role       = aws_iam_role.lambda_exec_role[0].name
+  policy_arn = aws_iam_policy.lambda_sfn_start_exec[0].arn
 }
 
 ###############################
 # MWAA Trigger Permissions
 ###############################
 resource "aws_iam_policy" "lambda_trigger_mwaa_job" {
+  count = var.DEPLOY_VECTOR_AUTOMATION ? 1 : 0
+
   provider = aws.aws_current
   name        = "lambda-trigger-mwaa-veda-${var.stage}"
   path        = "/"
@@ -119,43 +139,51 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_trigger_mwaa_job" {
+  count = var.DEPLOY_VECTOR_AUTOMATION ? 1 : 0
+
   provider = aws.aws_current
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = aws_iam_policy.lambda_trigger_mwaa_job.arn
+  role       = aws_iam_role.lambda_exec_role[0].name
+  policy_arn = aws_iam_policy.lambda_trigger_mwaa_job[0].arn
 }
 
 #####################################################
 # Lambda
 #####################################################
 data "archive_file" "archive" {
+  count = var.DEPLOY_VECTOR_AUTOMATION ? 1 : 0
+
   type        = "zip"
   source_dir  = "functions/s3_event_bridge_to_sfn_execute"
   output_path = "s3_event_bridge_to_sfn_execute.zip"
 }
 
 resource "aws_lambda_function" "lambda" {
+  count = var.DEPLOY_VECTOR_AUTOMATION ? 1 : 0
+
   provider = aws.aws_current
   filename         = "s3_event_bridge_to_sfn_execute.zip"
   function_name    = "s3-event-bridge-to-sfn-execute-veda-${var.stage}"
-  role             = aws_iam_role.lambda_exec_role.arn
+  role             = aws_iam_role.lambda_exec_role[0].arn
   handler          = "lambda_function.lambda_handler"
-  source_code_hash = data.archive_file.archive.output_base64sha256
+  source_code_hash = data.archive_file.archive[0].output_base64sha256
   runtime          = "python3.9"
   publish          = true
 
   environment {
     variables = {
-      LOG_GROUP_NAME = "/aws/lambda/s3-event-bridge-to-sfn-execute-veda-${var.stage}"
-      TARGET_MWAA_ENV = "veda-pipeline-${var.stage}-mwaa"
-      TARGET_DAG_ID = "veda_discover"
+      LOG_GROUP_NAME     = "/aws/lambda/s3-event-bridge-to-sfn-execute-veda-${var.stage}"
+      TARGET_MWAA_ENV    = "veda-pipeline-${var.stage}-mwaa"
+      TARGET_DAG_ID      = "veda_discover"
       TARGET_DAG_COMMAND = "dags trigger"
     }
   }
 }
 
 resource "aws_cloudwatch_log_group" "group" {
+  count = var.DEPLOY_VECTOR_AUTOMATION ? 1 : 0
+
   provider = aws.aws_current
-  name              = "/aws/lambda/${aws_lambda_function.lambda.function_name}"
+  name              = "/aws/lambda/${aws_lambda_function.lambda[0].function_name}"
   retention_in_days = 5
 }
 
@@ -163,9 +191,11 @@ resource "aws_cloudwatch_log_group" "group" {
 # RESOURCE POLICY for EVENT INVOCATION
 #####################################################
 resource "aws_lambda_permission" "s3_invoke" {
+  count = var.DEPLOY_VECTOR_AUTOMATION ? 1 : 0
+
   provider = aws.aws_current
   action           = "lambda:InvokeFunction"
-  function_name    = aws_lambda_function.lambda.function_name
+  function_name    = aws_lambda_function.lambda[0].function_name
   principal        = "s3.amazonaws.com"
   statement_id     = "AllowInvocationFromS3Bucket-veda-${var.stage}"
   source_account   = "114506680961"

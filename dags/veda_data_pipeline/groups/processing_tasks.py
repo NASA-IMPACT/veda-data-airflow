6d3e29a0201d58a62fb2d17d73de5f9523b634dc
Abdelhak Marouane <am0089@uah.edu>
2024-10-24 09:06:22 -0500

    Using pythonoperators instead of ECS operator

--- dags/veda_data_pipeline/groups/processing_tasks.py
+++ dags/veda_data_pipeline/groups/processing_tasks.py
@@ -1,12 +1,9 @@
 from datetime import timedelta
 import json
 import logging
-
 import smart_open
 from airflow.models.variable import Variable
-from airflow.operators.python import PythonOperator
-from airflow.providers.amazon.aws.operators.ecs import EcsRunTaskOperator
-from airflow.decorators import task_group, task
+from airflow.decorators import task
 from veda_data_pipeline.utils.submit_stac import submission_handler
 
 group_kwgs = {"group_id": "Process", "tooltip": "Process"}
@@ -15,11 +12,17 @@ group_kwgs = {"group_id": "Process", "tooltip": "Process"}
 def log_task(text: str):
     logging.info(text)
 
+
 @task(retries=1, retry_delay=timedelta(minutes=1))
-def submit_to_stac_ingestor_task(built_stac:str):
+def submit_to_stac_ingestor_task(built_stac: dict):
     """Submit STAC items to the STAC ingestor API."""
-    event = json.loads(built_stac)
+    event = built_stac.copy()
     success_file = event["payload"]["success_event_key"]
+
+    airflow_vars = Variable.get("aws_dags_variables")
+    airflow_vars_json = json.loads(airflow_vars)
+    cognito_app_secret = airflow_vars_json.get("COGNITO_APP_SECRET")
+    stac_ingestor_api_url = airflow_vars_json.get("STAC_INGESTOR_API_URL")
     with smart_open.open(success_file, "r") as _file:
         stac_items = json.loads(_file.read())
 
@@ -27,201 +30,11 @@ def submit_to_stac_ingestor_task(built_stac:str):
         submission_handler(
             event=item,
             endpoint="/ingestions",
-            cognito_app_secret=Variable.get("COGNITO_APP_SECRET"),
-            stac_ingestor_api_url=Variable.get("STAC_INGESTOR_API_URL"),
+            cognito_app_secret=cognito_app_secret,
+            stac_ingestor_api_url=stac_ingestor_api_url,
         )
     return event
 
-@task
-def build_stac_kwargs(event={}):
-    """Build kwargs for the ECS operator."""
-    mwaa_stack_conf = Variable.get("MWAA_STACK_CONF", deserialize_json=True)
-    if event:
-        intermediate = {
-            **event
-        } # this is dumb but it resolves the MappedArgument to a dict that can be JSON serialized
-        payload = json.dumps(intermediate)
-    else:
-        payload = "{{ task_instance.dag_run.conf }}"
-
-    return {
-        "overrides": {
-            "containerOverrides": [
-                {
-                    "name": f"{mwaa_stack_conf.get('PREFIX')}-veda-stac-build",
-                    "command": [
-                        "/usr/local/bin/python",
-                        "handler.py",
-                        "--payload",
-                        payload,
-                    ],
-                    "environment": [
-                        {
-                            "name": "EXTERNAL_ROLE_ARN",
-                            "value": Variable.get(
-                                "ASSUME_ROLE_READ_ARN", default_var=""
-                            ),
-                        },
-                        {
-                            "name": "BUCKET",
-                            "value": "veda-data-pipelines-staging-lambda-ndjson-bucket",
-                        },
-                        {
-                            "name": "EVENT_BUCKET",
-                            "value": mwaa_stack_conf.get("EVENT_BUCKET"),
-                        }
-                    ],
-                    "memory": 2048,
-                    "cpu": 1024,
-                },
-            ],
-        },
-        "network_configuration": {
-            "awsvpcConfiguration": {
-                "securityGroups": mwaa_stack_conf.get("SECURITYGROUPS"),
-                "subnets": mwaa_stack_conf.get("SUBNETS"),
-            },
-        },
-        "awslogs_group": mwaa_stack_conf.get("LOG_GROUP_NAME"),
-        "awslogs_stream_prefix": f"ecs/{mwaa_stack_conf.get('PREFIX')}-veda-stac-build",
-    }
-
-@task
-def build_vector_kwargs(event={}):
-    """Build kwargs for the ECS operator."""
-    mwaa_stack_conf = Variable.get(
-        "MWAA_STACK_CONF", default_var={}, deserialize_json=True
-    )
-    vector_ecs_conf = Variable.get(
-        "VECTOR_ECS_CONF", default_var={}, deserialize_json=True
-    )
-
-    if event:
-        intermediate = {
-            **event
-        }
-        payload = json.dumps(intermediate)
-    else:
-        payload = "{{ task_instance.dag_run.conf }}"
-    
-    return {
-        "overrides": {
-            "containerOverrides": [
-                {
-                    "name": f"{mwaa_stack_conf.get('PREFIX')}-veda-vector_ingest",
-                    "command": [
-                        "/var/lang/bin/python",
-                        "handler.py",
-                        "--payload",
-                        payload,
-                    ],
-                    "environment": [
-                        {
-                            "name": "EXTERNAL_ROLE_ARN",
-                            "value": Variable.get(
-                                "ASSUME_ROLE_READ_ARN", default_var=""
-                            ),
-                        },
-                        {
-                            "name": "AWS_REGION",
-                            "value": mwaa_stack_conf.get("AWS_REGION"),
-                        },
-                        {
-                            "name": "VECTOR_SECRET_NAME",
-                            "value": Variable.get("VECTOR_SECRET_NAME"),
-                        },
-                        {
-                            "name": "AWS_STS_REGIONAL_ENDPOINTS",
-                            "value": "regional",  # to override this behavior, make sure AWS_REGION is set to `aws-global`
-                        }
-                    ],
-                },
-            ],
-        },
-        "network_configuration": {
-            "awsvpcConfiguration": {
-                "securityGroups": vector_ecs_conf.get("VECTOR_SECURITY_GROUP"),
-                "subnets": vector_ecs_conf.get("VECTOR_SUBNETS"),
-            },
-        },
-        "awslogs_group": mwaa_stack_conf.get("LOG_GROUP_NAME"),
-        "awslogs_stream_prefix": f"ecs/{mwaa_stack_conf.get('PREFIX')}-veda-vector_ingest",
-    }
 
 
-@task
-def build_generic_vector_kwargs(event={}):
-    """Build kwargs for the ECS operator."""
-    mwaa_stack_conf = Variable.get(
-        "MWAA_STACK_CONF", default_var={}, deserialize_json=True
-    )
-    vector_ecs_conf = Variable.get(
-        "VECTOR_ECS_CONF", default_var={}, deserialize_json=True
-    )
 
-    if event:
-        intermediate = {
-            **event
-        }
-        payload = json.dumps(intermediate)
-    else:
-        payload = "{{ task_instance.dag_run.conf }}"
-    
-    return {
-        "overrides":{
-            "containerOverrides": [
-                {
-                    "name": f"{mwaa_stack_conf.get('PREFIX')}-veda-generic_vector_ingest",
-                    "command": [
-                        "/var/lang/bin/python",
-                        "handler.py",
-                        "--payload",
-                        payload,
-                    ],
-                    "environment": [
-                        {
-                            "name": "EXTERNAL_ROLE_ARN",
-                            "value": Variable.get(
-                                "ASSUME_ROLE_READ_ARN", default_var=""
-                            ),
-                        },
-                        {
-                            "name": "AWS_REGION",
-                            "value": mwaa_stack_conf.get("AWS_REGION"),
-                        },
-                        {
-                            "name": "VECTOR_SECRET_NAME",
-                            "value": Variable.get("VECTOR_SECRET_NAME"),
-                        },
-                        {
-                            "name": "AWS_STS_REGIONAL_ENDPOINTS",
-                            "value": "regional",  # to override this behavior, make sure AWS_REGION is set to `aws-global`
-                        }
-                    ],
-                },
-            ],
-        },
-        "network_configuration":{
-            "awsvpcConfiguration": {
-                "securityGroups": vector_ecs_conf.get("VECTOR_SECURITY_GROUP") + mwaa_stack_conf.get("SECURITYGROUPS"),
-                "subnets": vector_ecs_conf.get("VECTOR_SUBNETS"),
-            },
-        },
-        "awslogs_group":mwaa_stack_conf.get("LOG_GROUP_NAME"),
-        "awslogs_stream_prefix":f"ecs/{mwaa_stack_conf.get('PREFIX')}-veda-generic-vector_ingest",  # prefix with container name
-    }
-
-
-@task_group
-def subdag_process(event={}):
-
-    build_stac = EcsRunTaskOperator.partial(
-        task_id="build_stac"
-    ).expand_kwargs(build_stac_kwargs(event=event))
-
-    submit_to_stac_ingestor = PythonOperator(
-        task_id="submit_to_stac_ingestor",
-        python_callable=submit_to_stac_ingestor_task,
-    )
-
-    build_stac >> submit_to_stac_ingestor

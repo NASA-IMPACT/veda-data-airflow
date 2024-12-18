b32343b0c581934e17135189b94c931c360adbb8
ividito <isayah@developmentseed.org>
2024-12-18 15:20:59 -0400

    Break out promotion to new DAG

---
+++ dags/veda_data_pipeline/veda_promotion_pipeline.py
@@ -0,0 +1,141 @@
+import pendulum
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.dummy_operator import DummyOperator as EmptyOperator
+from airflow.models.variable import Variable
+import json
+from veda_data_pipeline.groups.collection_group import collection_task_group
+from veda_data_pipeline.groups.discover_group import discover_from_s3_task, get_dataset_files_to_process
+from veda_data_pipeline.groups.processing_tasks import submit_to_stac_ingestor_task
+from veda_data_pipeline.groups.transfer_group import transfer_data
+
+
+dag_doc_md = """
+### Dataset Pipeline
+Generates a collection and triggers the file discovery process
+#### Notes
+- This DAG can run with the following configuration <br>
+```json
+{
+    "collection": "collection-id", 
+    "data_type": "cog", 
+    "description": "collection description", 
+    "discovery_items": 
+        [
+            {
+                "bucket": "veda-data-store-staging", 
+                "datetime_range": "year", 
+                "discovery": "s3", 
+                "filename_regex": "^(.*).tif$", 
+                "prefix": "example-prefix/"
+            }
+        ], 
+    "is_periodic": true, 
+    "license": "collection-LICENSE", 
+    "time_density": "year", 
+    "title": "collection-title"
+}
+```
+"""
+
+dag_args = {
+    "start_date": pendulum.today("UTC").add(days=-1),
+    "schedule_interval": None,
+    "catchup": False,
+    "doc_md": dag_doc_md,
+    "tags": ["collection", "discovery"],
+}
+
+
+@task
+def extract_discovery_items(**kwargs):
+    ti = kwargs.get("ti")
+    discovery_items = ti.dag_run.conf.get("discovery_items")
+    print(discovery_items)
+    return discovery_items
+
+
+@task(max_active_tis_per_dag=3)
+def build_stac_task(payload):
+    from veda_data_pipeline.utils.build_stac.handler import stac_handler
+    airflow_vars = Variable.get("aws_dags_variables")
+    airflow_vars_json = json.loads(airflow_vars)
+    event_bucket = airflow_vars_json.get("EVENT_BUCKET")
+    return stac_handler(payload_src=payload, bucket_output=event_bucket)
+
+@task()
+def mutate_payload(**kwargs):
+    ti = kwargs.get("ti")
+    payload = ti.dag_run.conf
+    if assets := payload.get("assets"):
+        # remove thumbnail asset if provided in collection config
+        if "thumbnail" in assets.keys():
+            assets.pop("thumbnail")
+        # if thumbnail was only asset, delete assets
+        if not assets:
+            payload.pop("assets")
+        # finally put the mutated assets back in the payload
+        else:
+            payload["assets"] = assets
+    return payload
+
+@task(max_active_tis_per_dag=3)
+def transfer_assets_to_production_bucket(payload):
+    transfer_data(payload)
+    # if transfer complete, update discovery payload to reflect new bucket
+    payload.update({"bucket": "veda-data-store"})
+    payload.update({"prefix": payload.get("collection")+"/"})
+    return payload
+
+
+template_dag_run_conf = {
+    "collection": "<collection-id>",
+    "data_type": "cog",
+    "description": "<collection-description>",
+    "discovery_items":
+        [
+            {
+                "bucket": "<bucket-name>",
+                "datetime_range": "<range>",
+                "discovery": "s3",
+                "filename_regex": "<regex>",
+                "prefix": "<example-prefix/>"
+            }
+        ],
+    "is_periodic": "<true|false>",
+    "license": "<collection-LICENSE>",
+    "time_density": "<time-density>",
+    "title": "<collection-title>",
+    "transfer": "<true|false> # transfer assets to production bucket if true (false by default)", 
+}
+
+with DAG("veda_dataset_pipeline", params=template_dag_run_conf, **dag_args) as dag:
+    # ECS dependency variable
+
+    start = EmptyOperator(task_id="start", dag=dag)
+    end = EmptyOperator(task_id="end", dag=dag)
+
+    collection_grp = collection_task_group()
+    mutate_payload_task = mutate_payload()
+    extract_from_payload = extract_discovery_items()
+
+    # asset transfer to production bucket
+    transfer_flag = dag.params.get("transfer", False)
+    if transfer_flag:
+        transfer_task = transfer_assets_to_production_bucket.expand(payload=extract_from_payload)
+        discover = discover_from_s3_task.partial(alt_payload=mutate_payload_task).expand(event=transfer_task)
+    else:
+        discover = discover_from_s3_task.partial(alt_payload=mutate_payload_task).expand(event=extract_from_payload)
+    discover.set_upstream(collection_grp)  # do not discover until collection exists
+
+    get_files = get_dataset_files_to_process(payload=discover) # untangle mapped data format to get iterable payloads from discover step
+
+    
+    build_stac = build_stac_task.expand(payload=get_files)
+    build_stac.set_upstream(get_files)
+    # .output is needed coming from a non-taskflow operator
+    submit_stac = submit_to_stac_ingestor_task.expand(built_stac=build_stac)
+
+    collection_grp.set_upstream(start)
+    mutate_payload_task.set_upstream(start)
+    submit_stac.set_downstream(end)

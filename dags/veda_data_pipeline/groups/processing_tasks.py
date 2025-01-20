2930826dd9f08fca1a77ed63ce08867f1e175ee2
ividito <isayah@developmentseed.org>
2025-01-20 15:46:00 -0400

    fix(submit-stac): modify retry behavior for API submission

--- dags/veda_data_pipeline/groups/processing_tasks.py
+++ dags/veda_data_pipeline/groups/processing_tasks.py
@@ -29,7 +29,8 @@ def remove_thumbnail_asset(ti):
         payload.pop("assets")
     return payload
 
-@task(retries=1, retry_delay=timedelta(minutes=1))
+# with exponential backoff enabled, retry delay is converted to seconds
+@task(retries=2, retry_delay=60, retry_exponential_backoff=True, max_active_tis_per_dag=5)
 def submit_to_stac_ingestor_task(built_stac: dict):
     """Submit STAC items to the STAC ingestor API."""
     event = built_stac.copy()

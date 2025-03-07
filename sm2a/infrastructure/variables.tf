769c3fe4102eaf7c16fa6a69343f5011f367e0f8
Abdelhak Marouane <am0089@uah.edu>
2025-03-07 09:35:26 -0600

    Fix policy

--- sm2a/infrastructure/variables.tf
+++ sm2a/infrastructure/variables.tf
@@ -100,7 +100,7 @@ variable "custom_worker_policy_statement" {
     },
     {
             "Effect": "Allow",
-            "Action": "cloudfront:CreateInvalidation",
+            "Action": ["cloudfront:CreateInvalidation"],
             "Resource": "arn:aws:cloudfront::*:distribution/*"
     }
 

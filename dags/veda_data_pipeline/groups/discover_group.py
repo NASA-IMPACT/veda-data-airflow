b49c51ae3fcd781f4eec1e83f1fafadd2bc2ab5a
Alexandra Kirk <alexandra@developmentseed.org>
2025-03-13 08:24:11 -0600

    Update dags/veda_data_pipeline/groups/discover_group.py

--- dags/veda_data_pipeline/groups/discover_group.py
+++ dags/veda_data_pipeline/groups/discover_group.py
@@ -14,7 +14,7 @@ group_kwgs = {"group_id": "Discover", "tooltip": "Discover"}
 
 
 @task(retries=1, retry_delay=timedelta(minutes=1))
-def discover_from_s3_task(*, event: dict = {}, ti=None, payload: dict ={}, prev_start_date_success: str = None):
+def discover_from_s3_task(*, event: dict={}, ti=None, payload: dict={}, prev_start_date_success: str=None):
     """Discover grouped assets/files from S3 in batches of 2800. Produce a list of such files stored on S3 to process.
     This task is used as part of the discover_group subdag and outputs data to EVENT_BUCKET.
     """

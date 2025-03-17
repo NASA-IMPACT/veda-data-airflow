5dc13ff23f9101e096893aabbb2310631aba69d7
Paridhi <073bct525.paridhi@pcampus.edu.np>
2025-03-17 16:32:52 -0500

    ...

--- sm2a/infrastructure/main.tf
+++ sm2a/infrastructure/main.tf
@@ -91,7 +91,7 @@ module "sma-base" {
     VECTOR_SECRET_NAME    = var.vector_secret_name
     ASSUME_ROLE_READ_ARN = var.assume_role_read_arn
     ASSUME_ROLE_WRITE_ARN = var.assume_role_write_arn
-    SM2A_BASE_URL = module.sma-base.airflow_url
+    SM2A_BASE_URL         = module.sma-base.airflow_url
     GLUE_ROLE_ARN = aws_iam_role.glue_crawler_role.arn,
     S3_EXPORT_KMS_KEY_ID = aws_kms_key.s3_export_kms_key.id,
     S3_EXPORT_ROLE_ARN = aws_iam_role.snapshot_export_role.arn,

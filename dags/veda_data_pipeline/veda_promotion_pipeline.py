bec98cf4513126eaf231bb5ba1b21ff1aeafd70b
Jennifer Tran <12633533+botanical@users.noreply.github.com>
2025-04-04 13:45:23 -0700

    fix: follow convention and check value in payload and then check dag run conf

--- dags/veda_data_pipeline/veda_promotion_pipeline.py
+++ dags/veda_data_pipeline/veda_promotion_pipeline.py
@@ -80,7 +80,7 @@ def transfer_assets_to_production_bucket(ti=None, payload={}):
         "origin_prefix": payload.get("prefix", ti.dag_run.conf.get("origin_prefix", "s3-prefix/")),
         "target_bucket": payload.get("target_bucket", ti.dag_run.conf.get("target_bucket", "veda-data-store")),
         "dry_run": payload.get("dry_run", ti.dag_run.conf.get("dry_run", False)),
-        "transfer": ti.dag_run.conf.get("transfer"),
+        "transfer": payload.get("transfer", ti.dag_run.conf.get("transfer", False)),
     }
     transfer_data(payload=config)
     # if transfer complete, update discovery payload to reflect new bucket

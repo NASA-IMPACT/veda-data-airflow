a2f33223fa1a58f792cdc0cefc92d5ba57878888
ividito <isayah@developmentseed.org>
2025-04-15 10:46:34 -0300

    More MWAA removal

--- scripts/sync-env-local.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Use this script to load environment variables for a deployment from AWS Secrets
-
-echo Loading environment secrets from $1
-aws secretsmanager get-secret-value --secret-id $1 --query SecretString --output text | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > .env

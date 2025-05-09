840c3ca509bbd7a461eac89133748c3f4773c83e
ividito <isayah@developmentseed.org>
2025-05-09 17:09:58 -0300

    Clean up airflow cmd execution - remove schedule on stactools DAGs - fix SIT deployment

--- dags/veda_data_pipeline/veda_stactools_noaa_hrrr.py
+++ dags/veda_data_pipeline/veda_stactools_noaa_hrrr.py
@@ -72,7 +72,7 @@ def get_stactools_dag(id, event={}):
     params_dag_run_conf = event or template_dag_run_conf
     with DAG(
         id,
-        schedule_interval=event.get("schedule"),
+        schedule_interval=event.get("schedule", None),
         params=params_dag_run_conf,
         **dag_args
     ) as dag:

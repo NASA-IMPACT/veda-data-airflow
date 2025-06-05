9264cc3a8789cbce5b314611652350d68371a6de
Sanjog Thapa <sanzog03@gmail.com>
2025-06-05 11:47:11 -0500

    refactor dag to use taskflow api completely. added docstring to the tasks. also extracted key variables as global for reusability

--- dags/veda_data_pipeline/veda_worldview_nrt_collection_update_pipeline.py
+++ dags/veda_data_pipeline/veda_worldview_nrt_collection_update_pipeline.py
@@ -5,7 +5,7 @@ import datetime
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.utils.trigger_rule import TriggerRule
-from airflow.decorators import task, task_group
+from airflow.decorators import dag, task, task_group
 from veda_data_pipeline.groups.collection_group import ingest_collection_task
 
 VIIRS_SNPP_NRT_collection = {
@@ -83,6 +83,10 @@ VIIRS_SNPP_NRT_collection = {
         "units": "m·s⁻¹"
     }
 
+gibs_url = "https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/1.0.0/WMTSCapabilities.xml"
+
+collection_id = "VIIRS_SNPP_DayNightBand_At_Sensor_Radiance"
+
 dag_doc_md = f"""
 ### This DAG handles VIIRS_SNPP_DayNightBand_At_Sensor_Radiance NRT dataset update.
 It checks if the NRT data hosted by earthdata is avaialble for the latest available date
@@ -103,122 +107,204 @@ dag_args = {
     "tags": ["collection"],
 }
 
-# Special task group to update nightlight NRT data collection that is pulled from worldview
-@task_group(group_id="worldview_nightlight_nrt_collection_update_pipeline", tooltip="worldview nightlight NRT Collection update")
-def worldview_collection_update_task_group(**context):
-    nrt_collection = context.get("VIIRS_SNPP_NRT_collection")
-    gibs_url: str ="https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/1.0.0/WMTSCapabilities.xml"
-
-    @task_group(group_id="validation_task_group", tooltip="validate if collection update is needed via metadata available on gibs")
-    def validation_task_group(gibs_url: str) -> dict:
-        xml_string_data = fetch_nightlight_meta_from_gibs(gibs_url)
-        latest_layer_date = extract_latest_nrt_date(xml_string_data)
-        is_update_needed = nrt_update_check(latest_layer_date)
-        # the above TaskFlow API implementation represents: fetch_nightlight_meta_from_gibs >> extract_latest_nrt_date >> nrt_update_check
-        return {
-            "is_update_needed": is_update_needed,
-            "latest_layer_date": latest_layer_date
-        }
-
-    @task_group(group_id="collection_update_task_group", tooltip="update the nightlight nrt collection with the available nrt date")
-    def collection_update_task_group(nrt_collection: dict, latest_layer_date: str) -> None:
-        updated_collection = update_nrt_collection_task(nrt_collection, latest_layer_date)
-        ingest_collection_task(collection=updated_collection)
-
-    @task.branch
-    def update_needed_check(update_needed: bool) -> str:
-        """
-        This task branches to either end or to upadte_nrt_collection_task,
-        based on the provided string date "%Y-%m-%d".
-        """
-        if not update_needed:
-            return 'end'
-        else:
-            return 'worldview_nightlight_nrt_collection_update_pipeline.collection_update_task_group.update_nrt_collection_task'
-
-    validation_result = validation_task_group(gibs_url)
-    branch_choice_instance = update_needed_check(validation_result['is_update_needed'])
-    branch_choice_instance >> [end, collection_update_task_group(nrt_collection, validation_result['latest_layer_date'])]
-
-@task
-def nrt_update_check(nrt_date: str="") -> bool:
-    """
-    Check if a Near Real-Time (NRT) data is updated to latest/today.
-
-    This task compares a provided date string with the current date to determine
-    if the NRT data is up-to-date. The nrt_date parameter should be in the format
-    "%Y-%m-%d".
-    """
-    if (not nrt_date):
-        return False
-
-    year, month, day = map(int, nrt_date.split("-"))
-    latest_nrt_data_date = datetime.date(year, month, day)
-    today = datetime.date.today()
-    if (latest_nrt_data_date >= today):
-        return True
-    return False
-
-@task()
-def update_nrt_collection_task(previous_collection=None, latest_nrt_date=None):
-    import copy
-
-    if (not previous_collection or not latest_nrt_date):
-        return None
-
-    updated_collection = copy.deepcopy(previous_collection)
-    year, month, day = map(int, latest_nrt_date.split("-"))
-    latest_nrt_data_date = datetime.date(year, month, day)
-    formatted_datetime = latest_nrt_data_date.strftime("%Y-%m-%dT00:00:00Z")
-    updated_collection['extent']['temporal']['interval'][0][1] = formatted_datetime
-    return updated_collection
-
-@task
-def fetch_nightlight_meta_from_gibs(gibs_url: str="https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/1.0.0/WMTSCapabilities.xml") -> str:
-    try:
-        response = requests.get(gibs_url)
-        response.raise_for_status()
-        xml_string = response.text
-        return xml_string
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching from the gibs: {e}")
-        return ""
-
-@task
-def extract_latest_nrt_date(xml_string: str) -> str:
-    import xml.etree.ElementTree as ET
-    XML_NAMESPACE = {'xmlns': 'http://www.opengis.net/wmts/1.0'}
-    OWS_NAMESPACE = {'ows': 'http://www.opengis.net/ows/1.1'}
-
-    if not xml_string:
-        return ""
-
-    root = ET.fromstring(xml_string)
-    contents = root.find('xmlns:Contents', XML_NAMESPACE)
-    if contents is None:
-        return False
-    layers = contents.findall('xmlns:Layer', XML_NAMESPACE)
-    if not layers:
-        return False
-    for layer in layers:
-        layer_id = layer.find('ows:Identifier', OWS_NAMESPACE).text
-        if (layer_id == 'VIIRS_SNPP_DayNightBand_At_Sensor_Radiance'):
-            dimension = layer.find('xmlns:Dimension', XML_NAMESPACE)
-            dimension_id = dimension.find('ows:Identifier', OWS_NAMESPACE).text
-            if (dimension_id == 'Time'):
-                layer_date = dimension.find('xmlns:Default', XML_NAMESPACE).text
-                return layer_date
-    return ""
-
-with DAG(
-    "veda_worldview_nrt_data_collection_update",
+@dag(
+    dag_id="veda_worldview_nrt_data_collection_update",
     schedule="0 0 * * *",
     render_template_as_native_obj=True,
     **dag_args
-) as dag:
-    start = EmptyOperator(task_id="start", dag=dag)
-    end = EmptyOperator(task_id="end", trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS, dag=dag)
+)
+def veda_worldview_nrt_data_collection_update():
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end", trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS)
 
-    collection_grp = worldview_collection_update_task_group(VIIRS_SNPP_NRT_collection=VIIRS_SNPP_NRT_collection)
+    # TASK DEFINATION START
 
+    @task_group(group_id="worldview_nightlight_nrt_collection_update_pipeline", tooltip="worldview nightlight NRT Collection update")
+    def worldview_collection_update_task_group(nrt_collection: dict, gibs_url: str ="https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/1.0.0/WMTSCapabilities.xml") -> None:
+        """
+        Task group to manage the update of the Nightlight Near Real-Time (NRT) data collection sourced from Worldview.
+
+        This task group consists of tasks that:
+        1. Validate if an update is needed by comparing the latest available data on GIBS with the current collection.
+        2. If an update is needed, it updates the NRT collection with the latest available date.
+        3. Finally, it ingests the updated collection.
+
+        The sub-tasks are grouped into validation and collection update task groups for better organization and readability.
+        A branching task determines whether to proceed with the update based on the validation result.
+        :param nrt_collection: A collection config dictionary.
+        :param gibs_url: The URL to the GIBS metadata endpoint.
+        :return: None
+        """
+        @task_group(group_id="validation_task_group", tooltip="validate if collection update is needed via metadata available on gibs")
+        def validation_task_group(gibs_url: str) -> dict:
+            """
+            Task group to validate if a collection update is needed based on metadata from GIBS.
+
+            This group fetches metadata from GIBS which is in XML format, extracts the latest date, and checks if an update is needed.
+
+            :param gibs_url: The URL to the GIBS metadata endpoint.
+            :return: A dictionary containing a boolean indicating if an update is needed and the latest layer date.
+            """
+            xml_string_data = fetch_nightlight_meta_from_gibs(gibs_url)
+            latest_layer_date = extract_latest_nrt_date(xml_string_data, collection_id)
+            is_update_needed = nrt_update_check(latest_layer_date)
+            # the above TaskFlow API implementation represents: fetch_nightlight_meta_from_gibs >> extract_latest_nrt_date >> nrt_update_check
+            return {
+                "is_update_needed": is_update_needed,
+                "latest_layer_date": latest_layer_date
+            }
+
+        @task_group(group_id="collection_update_task_group", tooltip="update the nightlight nrt collection with the available nrt date")
+        def collection_update_task_group(nrt_collection: dict, latest_layer_date: str) -> None:
+            """Updates the NRT collection config json with the latest available date and ingests it.
+
+            :param nrt_collection: Dictionary representing the NRT collection config.
+            :param latest_layer_date: The latest date for which NRT data is available.
+            :return: None
+            """
+            updated_collection = update_nrt_collection_task(nrt_collection, latest_layer_date)
+            ingest_collection_task(collection=updated_collection)
+
+        @task.branch
+        def update_needed_check(update_needed: bool) -> str:
+            """
+            This task branches to either end or to upadte_nrt_collection_task,
+            based on the provided boolean representing if updated is needed or not.
+            :param update_needed: Boolean
+            """
+            if not update_needed:
+                return 'end'
+            else:
+                return 'worldview_nightlight_nrt_collection_update_pipeline.collection_update_task_group.update_nrt_collection_task'
+
+        validation_result = validation_task_group(gibs_url)
+        branch_choice_instance = update_needed_check(validation_result['is_update_needed'])
+        branch_choice_instance >> [end, collection_update_task_group(nrt_collection, validation_result['latest_layer_date'])]
+
+    @task
+    def nrt_update_check(nrt_date: str="") -> bool:
+        """
+        Check if Near Real-Time (NRT) data is up-to-date.
+
+        Compares a provided date string with the current date to determine
+        if the NRT data is up-to-date. The nrt_date parameter should be in the format
+        "%Y-%m-%d". If nrt_date is empty, it returns False.
+
+        :param nrt_date: Date string in the format "%Y-%m-%d" representing the latest NRT data date.
+        :type nrt_date: str, optional
+        :return: True if the NRT data is up-to-date (nrt_date is today or a future date), False otherwise.
+        :rtype: bool
+        """
+        if (not nrt_date):
+            return False
+
+        year, month, day = map(int, nrt_date.split("-"))
+        latest_nrt_data_date = datetime.date(year, month, day)
+        today = datetime.date.today()
+        if (latest_nrt_data_date >= today):
+            return True
+        return False
+
+    @task()
+    def update_nrt_collection_task(previous_collection=None, latest_nrt_date=None):
+        """
+        Updates the temporal extent of a collection with the latest NRT (Near Real-Time) data date.
+
+        This task takes the previous collection metadata and the latest available date for NRT data,
+        and updates the collection's temporal extent to include this new data.
+
+        :param previous_collection: The previous collection config as a dictionary.
+        :type previous_collection: dict, optional
+        :param latest_nrt_date: The latest date for which NRT data is available, in 'YYYY-MM-DD' format.
+                                 Defaults to None.
+        :type latest_nrt_date: str, optional
+        :return: The updated collection config with the temporal extent updated to include the
+                 latest NRT data date.
+        :rtype: dict or None
+        """
+        import copy
+
+        if (not previous_collection or not latest_nrt_date):
+            return None
+
+        updated_collection = copy.deepcopy(previous_collection)
+        year, month, day = map(int, latest_nrt_date.split("-"))
+        latest_nrt_data_date = datetime.date(year, month, day)
+        formatted_datetime = latest_nrt_data_date.strftime("%Y-%m-%dT00:00:00Z")
+        updated_collection['extent']['temporal']['interval'][0][1] = formatted_datetime
+        return updated_collection
+
+    @task
+    def fetch_nightlight_meta_from_gibs(gibs_url: str="https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/1.0.0/WMTSCapabilities.xml") -> str:
+        """
+        Fetches the nightlight metadata from the NASA Global Imagery Browse Services (GIBS) server.
+
+        Args:
+            gibs_url (str): The URL for the GIBS WMTSCapabilities.xml endpoint.
+                             Defaults to the public NASA GIBS endpoint for EPSG:4326.
+
+        Returns:
+            str: A string containing the XML response from the GIBS server,
+                 or an empty string if the fetch fails.
+
+        Raises:
+            requests.exceptions.RequestException: If the HTTP request to the GIBS server fails.
+        """
+        try:
+            response = requests.get(gibs_url)
+            response.raise_for_status()
+            xml_string = response.text
+            return xml_string
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching from the gibs: {e}")
+            return ""
+
+    @task
+    def extract_latest_nrt_date(xml_string: str, collection_id:str='VIIRS_SNPP_DayNightBand_At_Sensor_Radiance') -> str:
+        """
+        Extracts the latest date from the WMTS GetCapabilities XML response for a specified layer.
+
+        This function parses an XML string to find the most recent date available for a given
+        collection ID within the WMTS GetCapabilities response. It uses the XML namespace
+        to correctly locate the relevant elements and attributes.
+
+        :param xml_string: A string containing the XML response from the WMTS GetCapabilities request.
+        :type xml_string: str
+        :param collection_id: The identifier of the layer for which to extract the latest date.
+            Defaults to 'VIIRS_SNPP_DayNightBand_At_Sensor_Radiance'.
+        :type collection_id: str, optional
+        :return: The latest date as a string, extracted from the XML. Returns an empty string if the XML is empty,
+                or if the specified layer or date information is not found.
+        :rtype: str
+        :raises ET.ParseError: If the `xml_string` is not a valid XML.
+        """
+        import xml.etree.ElementTree as ET
+        XML_NAMESPACE = {'xmlns': 'http://www.opengis.net/wmts/1.0'}
+        OWS_NAMESPACE = {'ows': 'http://www.opengis.net/ows/1.1'}
+
+        if not xml_string:
+            return ""
+
+        root = ET.fromstring(xml_string)
+        contents = root.find('xmlns:Contents', XML_NAMESPACE)
+        if contents is None:
+            return False
+        layers = contents.findall('xmlns:Layer', XML_NAMESPACE)
+        if not layers:
+            return False
+        for layer in layers:
+            layer_id = layer.find('ows:Identifier', OWS_NAMESPACE).text
+            if (layer_id == collection_id):
+                dimension = layer.find('xmlns:Dimension', XML_NAMESPACE)
+                dimension_id = dimension.find('ows:Identifier', OWS_NAMESPACE).text
+                if (dimension_id == 'Time'):
+                    layer_date = dimension.find('xmlns:Default', XML_NAMESPACE).text
+                    return layer_date
+        return ""
+
+    # TASK DEFINATION END
+
+    collection_grp = worldview_collection_update_task_group(nrt_collection=VIIRS_SNPP_NRT_collection, gibs_url=gibs_url)
     start >> collection_grp >> end
+
+veda_worldview_nrt_data_collection_update()

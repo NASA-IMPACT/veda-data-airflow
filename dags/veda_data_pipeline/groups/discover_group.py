eca3554a40f425f47e28d3ed0d8b1c688cb458ca
ividito <isayah@developmentseed.org>
2025-06-24 16:43:46 -0300

    Add sm2a upgrade to support DB migration

--- dags/veda_data_pipeline/groups/discover_group.py
+++ dags/veda_data_pipeline/groups/discover_group.py
@@ -4,6 +4,7 @@ import uuid
 
 from airflow.models.variable import Variable
 from airflow.decorators import task
+from airflow.models.xcom import LazyXComSelectSequence
 from veda_data_pipeline.utils.s3_discovery import (
     s3_discovery_handler, EmptyFileListError
 )
@@ -60,7 +61,7 @@ def get_files_task(payload, ti=None):
     payloads = payload if isinstance(payload, list) else [payload]
 
     for item in payloads:
-        if isinstance(item, list):  # Dynamic task mapping case
+        if isinstance(item, LazyXComSelectSequence):  # Dynamic task mapping case
             payloads_xcom = item[0].pop("payload", [])
             base_payload = item[0]
         else:
@@ -82,7 +83,7 @@ def get_files_to_process(payload, ti=None):
     """Get files from S3 produced by the discovery task.
     Used as part of both the parallel_run_process_rasters and parallel_run_process_vectors tasks.
     """
-    if isinstance(payload, list):  # if used as part of a dynamic task mapping
+    if isinstance(payload, LazyXComSelectSequence):  # if used as part of a dynamic task mapping
         payloads_xcom = payload[0].pop("payload", [])
         payload = payload[0]
     else:
@@ -105,7 +106,7 @@ def get_dataset_files_to_process(payload, ti=None):
 
     result = []
     for x in payload:
-        if isinstance(x, list):  # if used as part of a dynamic task mapping
+        if isinstance(x, LazyXComSelectSequence):  # if used as part of a dynamic task mapping
             payloads_xcom = x[0].pop("payload", [])
             payload_0 = x[0]
         else:

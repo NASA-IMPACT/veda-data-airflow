f1fb195f87acaa7682c6af727d5aa864780d6971
Sanjog Thapa <sanzog03@gmail.com>
2025-11-17 14:29:40 -0600

    corrected import for tests

--- dags/generate_dags.py
+++ dags/generate_dags.py
@@ -7,7 +7,7 @@ from airflow.models.variable import Variable
 
 from veda_data_pipeline.veda_discover_pipeline import get_discover_dag
 from veda_data_pipeline.veda_vector_pipeline import get_ingest_vector_dag
-from dags.veda_data_pipeline.veda_wmts2stac_update_pipeline import get_ingest_wmts2stac_dag
+from veda_data_pipeline.veda_wmts2stac_update_pipeline import get_ingest_wmts2stac_dag
 from veda_data_pipeline.veda_pyarc2stac_pipeline import get_ingest_pyarc2stac_dag
 
 dag_generators = {
--- dags/veda_data_pipeline/veda_wmts2stac_update_pipeline.py
+++ dags/veda_data_pipeline/veda_wmts2stac_update_pipeline.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.utils.trigger_rule import TriggerRule
 from airflow.decorators import dag, task
-from dags.veda_data_pipeline.helpers.veda_wmts2stac_update_pipeline import gibs_wmts2stac_update_task_group, wmts2stac_task_group, validate_web_map_links_schema_task, VedaWMTS2STACConfig
+from veda_data_pipeline.helpers.veda_wmts2stac_update_pipeline import gibs_wmts2stac_update_task_group, wmts2stac_task_group, validate_web_map_links_schema_task, VedaWMTS2STACConfig
 
 def get_ingest_wmts2stac_dag(id: str, event: VedaWMTS2STACConfig) -> DAG:
     """

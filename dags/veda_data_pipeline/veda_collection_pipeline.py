2a64e4db3a6d927e8083717cde267e21b0ad3ac5
ividito <isayah@developmentseed.org>
2026-06-10 21:27:29 -0700

    demo: force change

--- dags/veda_data_pipeline/veda_collection_pipeline.py
+++ dags/veda_data_pipeline/veda_collection_pipeline.py
@@ -5,7 +5,7 @@ from airflow.utils.trigger_rule import TriggerRule
 from veda_data_pipeline.groups.collection_group import collection_task_group
 from slack_notifications import slack_fail_alert
 
-"""
+IMPORTANT = """
 This is a change to show the new versioned DAG feature. This comment is only on the new version!
 """
 

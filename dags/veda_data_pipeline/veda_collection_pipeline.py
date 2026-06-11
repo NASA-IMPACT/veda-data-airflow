06301404e13fb3ed2a0ea33d2f37eef770670df2
ividito <isayah@developmentseed.org>
2026-06-10 21:41:19 -0700

    demo: last change

--- dags/veda_data_pipeline/veda_collection_pipeline.py
+++ dags/veda_data_pipeline/veda_collection_pipeline.py
@@ -6,7 +6,9 @@ from veda_data_pipeline.groups.collection_group import collection_task_group
 from slack_notifications import slack_fail_alert
 
 IMPORTANT = """
-This is a change to show the new versioned DAG feature. This comment is only on the new version!
+This is a change to show the new versioned DAG feature.
+
+This is another change, on another DAG version.
 """
 
 dag_doc_md = """

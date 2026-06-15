b7db522fd4022184e8e78f08b55db9e545dd6554
ividito <isayah@developmentseed.org>
2026-06-15 11:16:01 -0700

    fix: bump sm2a module v1.2.0-rc3 -> rc4

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -49,7 +49,7 @@ locals {
 }
 
 module "sma-base" {
-  source                         = "https://github.com/NASA-IMPACT/self-managed-apache-airflow/releases/download/v1.2.0-rc3/self-managed-apache-airflow.zip"
+  source                         = "https://github.com/NASA-IMPACT/self-managed-apache-airflow/releases/download/v1.2.0-rc4/self-managed-apache-airflow.zip"
   project                        = var.project_name
   airflow_db                     = var.airflow_db
   fernet_key                     = var.fernet_key
